119804b08707760335f2e5afba5bffb57265778c
Suhan Koh <suhankoh@gmail.com>
2016-02-14 13:31:15 -0800

    Test new precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.precompile += %w( jquery.ui.chatbox.js )
+Rails.application.config.assets.precompile += %w( gamelobbies.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.js )
+Rails.application.config.assets.precompile += %w( jquery.ui.chatbox.js )
+Rails.application.config.assets.precompile += %w( login.js )
+Rails.application.config.assets.precompile += %w( npm.js )
+Rails.application.config.assets.precompile += %w( pusher.js )
 Rails.application.config.assets.precompile += %w( chat.css )
 Rails.application.config.assets.precompile += %w( gamechats.js )
 # Add additional assets to the asset load path
@@ -13,3 +19,7 @@ Rails.application.config.assets.precompile += %w( gamechats.js )
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( gamelobbies.css )
+Rails.application.config.assets.precompile += %w( bootstrap-theme.min.css )
+Rails.application.config.assets.precompile += %w( boostrap.min.css )
+Rails.application.config.assets.precompile += %w( jquery-ui.css )
+Rails.application.config.assets.precompile += %w( site.css )
